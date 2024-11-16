321798b5039260ee46a25873ae944f6fee0af610
Ryan Klein <ryan@ryanklein.dev>
2024-11-16 10:32:53 -0500

    Initial Commit

---
+++ NutritionCamera/NutritionCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionCameraApp.swift
+//  NutritionCamera
+//
+//  Created by Ryan Klein on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NutritionCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NutritionCameraTests/NutritionCameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionCameraTests.swift
+//  NutritionCameraTests
+//
+//  Created by Ryan Klein on 11/16/24.
+//
+
+import Testing
+@testable import NutritionCamera
+
+struct NutritionCameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
