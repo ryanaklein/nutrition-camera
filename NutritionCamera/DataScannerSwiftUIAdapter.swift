21314f9448f35a1beb55be1488ffb03d76c5c02a
Ryan Klein <ryan@ryanklein.dev>
2024-12-01 10:17:06 -0500

    DataScanner Commit I discovered that the DataScanner is now legacy. There is an updated version of Vision and I am now going adapt https://developer.apple.com/documentation/vision/locating-and-displaying-recognized-text.

--- NutritionCamera/DataScannerSwiftUIAdapter.swift
+++ NutritionCamera/DataScannerSwiftUIAdapter.swift
@@ -5,6 +5,7 @@
 //  Created by Ryan Klein on 11/16/24.
 //
 
+import Foundation
 import SwiftUI
 @preconcurrency import VisionKit
 
@@ -18,14 +19,13 @@ struct DataScannerSwiftUIAdapter: UIViewControllerRepresentable {
     }
  
     func makeUIViewController(context: Context) -> DataScannerViewController {
-        let viewController = DataScannerViewController(recognizedDataTypes: [.text()], qualityLevel: .fast, recognizesMultipleItems: true, isHighlightingEnabled: true)
+        let viewController = DataScannerViewController(recognizedDataTypes: [.text()], qualityLevel: .fast, recognizesMultipleItems: true, isHighlightingEnabled: false)
         context.coordinator.scanner = viewController
+        viewController.delegate = context.coordinator
 
         do {
             try viewController.startScanning()
-            Task {
-                await context.coordinator.updateViaAsyncStream()
-            }
+
         } catch {
             print(error.localizedDescription)
         }
@@ -45,51 +45,44 @@ struct DataScannerSwiftUIAdapter: UIViewControllerRepresentable {
         var task: Task<Void, Never>?
         let digits = try! Regex("^\\d+$")
         let calories = try! Regex("(?i)^calories$")
+        let encoder = JSONEncoder()
+        
         
         init(_ parent: DataScannerSwiftUIAdapter){
             self.parent = parent
+            encoder.outputFormatting = .prettyPrinted
+            
         }
         
         
-        func updateViaAsyncStream() async {
-            guard let scanner = scanner else {return}
-            
-            let stream = scanner.recognizedItems
-            
-            
-            for await newItems: [RecognizedItem] in stream {
-                let diff = newItems.difference(from: currentItems) { a, b in
-                    return a.id == b.id
-                }
-                
-                if !diff.isEmpty {
-                    currentItems = newItems
-                    
-                    var calorieDigitsFound = false
-                    
-                    for item in currentItems {
-                        switch item {
-                        case .text(let text):
-                            if text.transcript.firstMatch(of: calories) != nil{
-                                // "Calories" was found alone on a line. It's likely the next independent number is the actual calorie count. So, just continue.
-                                continue
-                            }
-                            if !calorieDigitsFound {
-                                if text.transcript.firstMatch(of: digits) != nil {
-                                    calorieDigitsFound = true
-                                }
-                            }
-                        default:
-                            continue
-                        }
-                    }
-                    if !calorieDigitsFound {
-                        parent.calories = "0"
-                    }
-                    
-                }
-            }
-        }
+//        func updateViaAsyncStream() async {
+//            guard let scanner = scanner else {return}
+//            
+//            let stream = scanner.recognizedItems
+//            
+//            
+//            for await newItems: [RecognizedItem] in stream {
+//
+//                if(newItems.count > 4){
+//                    scanner.stopScanning()
+//                    var foundTextList: [FoundText] = []
+//                    
+//                    for item in newItems {
+//                        switch item {
+//                        case .text(let text):
+//                            
+//                            for string in text.transcript.split(separator: " "){
+//                                foundTextList.append(FoundText(text: String(string), line: text.transcript, lineTop: Float(text.bounds.topLeft.x), lineBottom: Float(text.bounds.bottomLeft.x)))
+//                            }
+//                            
+//                            
+//                        default:
+//                            continue
+//                        }
+//                    }
+//                }
+//            }
+//        }
         
         func dataScanner(_ dataScanner: DataScannerViewController, didAdd addItems: [RecognizedItem], allItems: [RecognizedItem]) {
             guard let scanner = self.scanner else {return}
@@ -98,18 +91,22 @@ struct DataScannerSwiftUIAdapter: UIViewControllerRepresentable {
                 switch item {
                 case .text(let text):
                     
-                    if(text.transcript.firstMatch(of: digits) != nil && text.transcript.contains("Total Fat")){
+                    if(text.transcript.contains("Total Fat")){
                         
-                        print(text.transcript)
+                        scanner.stopScanning()
+                        
+                        
+                        Task {
+                            let image = try! await scanner.capturePhoto().cgImage
+                            
+                            
+                            
+                        }
+                        
                         
                         guard let totalFatTextRange = text.transcript.range(of: "Total") else {continue}
                         
-                        print(totalFatTextRange.lowerBound)
-                        print(totalFatTextRange.upperBound)
-                        
                         let topCandidate = text.observation.topCandidates(1)[0]
-                        
-                        print(topCandidate.string)
                         
                         guard let boundingBox = try? topCandidate.boundingBox(for: totalFatTextRange) else {continue}
                         
@@ -135,35 +132,36 @@ struct DataScannerSwiftUIAdapter: UIViewControllerRepresentable {
                 
                 switch item {
                 case .text(let text):
-                    if(text.transcript.firstMatch(of: digits) != nil && text.transcript.contains("Total Fat 22g")){
-                        
-                        print(text.transcript)
-                        
-                        guard let totalFatTextRange = text.transcript.range(of: "Total Fat 22g") else {continue}
-                        guard let totalSugarsTextRange = text.transcript.range(of: "Total Sugars 1g") else {continue}
-                        guard let totalCarbsTextRange = text.transcript.range(of: "Total Carbohydrate 4g") else {continue}
+                    if(text.transcript.contains("Total Fat") && text.transcript.contains("Cholesterol")){
                         
                         
-                        print(text.transcript[totalFatTextRange])
-                        
-                        print("\(totalFatTextRange.lowerBound)")
-                        print(totalFatTextRange.upperBound)
+                        guard let totalFatTextRange = text.transcript.range(of: "Total Fat") else {continue}
                         
                         
                         let topCandidate = text.observation.topCandidates(1)[0]
                         
                         print(topCandidate.string)
                         
-                        guard let boundingBox = try? topCandidate.boundingBox(for: String.Index(utf16Offset: 0, in: topCandidate.string)..<String.Index(utf16Offset: 100, in: topCandidate.string)) else {continue}
-                        guard let totalSugarsBoundingBox = try? topCandidate.boundingBox(for: totalSugarsTextRange) else {continue}
-                        guard let totalCarbsBoundingBox = try? topCandidate.boundingBox(for: totalCarbsTextRange) else {continue}
+                        guard let boundingBox = try? topCandidate.boundingBox(for: topCandidate.string.range(of: "Total Fat")!) else {continue}
+                        guard let cholesterolBoundingBox = try? topCandidate.boundingBox(for: topCandidate.string.range(of: "Cholesterol")!) else {continue}
                         
-                        print(boundingBox.topRight)
-                        print(totalSugarsBoundingBox.topRight)
-                        print(totalCarbsBoundingBox.topLeft)
+                        print("FAT")
+                        print(boundingBox.boundingBox)
+                        print(topCandidate.string.range(of: "Total Fat")!.lowerBound)
+                        print("Top left: \(boundingBox.topLeft.x), \(boundingBox.topLeft.y)")
+                        print("Bottom right: \(boundingBox.bottomRight.x), \(boundingBox.bottomRight.y)")
+                        print()
+                        print("CHOLESTEROL")
+                        print(cholesterolBoundingBox.boundingBox)
+                        print(topCandidate.string.range(of: "Cholesterol")!.lowerBound)
+                        print("Top left: \(cholesterolBoundingBox.topLeft.x), \(cholesterolBoundingBox.topLeft.y)")
+                        print("Bottom right: \(cholesterolBoundingBox.bottomRight.x), \(cholesterolBoundingBox.bottomRight.y)")
+                        print()
+                        print()
+                        
                         
                         if let hostingController = itemHighlightViews[item.id] {
-                            hostingController.view.frame = CGRect(origin: item.bounds.topLeft, size: CGSize(width: abs(boundingBox.topLeft.x - boundingBox.topRight.x), height: abs(boundingBox.topLeft.y - boundingBox.bottomRight.y)))
+                            hostingController.view.frame = CGRect(origin: item.bounds.topLeft, size: CGSize(width: abs(item.bounds.topLeft.x - item.bounds.topRight.x), height: abs(item.bounds.topLeft.y - item.bounds.bottomRight.y)))
                         }
                     }
                 default:
@@ -181,6 +179,59 @@ struct DataScannerSwiftUIAdapter: UIViewControllerRepresentable {
                     hostingController.removeFromParent()
                 }
             }
+        }
+
+        
+        func saveToFile(foundTextList: [String]){
+            // Define the file name and path
+            let fileName = "example.txt"
+            if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                let fileURL = dir.appendingPathComponent(fileName)
+                
+                do{
+                    try? FileManager.default.removeItem(at: fileURL)
+                }
+                
+                do{
+                    
+                    FileManager.default.createFile(atPath: fileURL.path(), contents: nil)
+                    let fileHandle = try FileHandle(forWritingTo: fileURL)
+                    for foundText in foundTextList {
+                        let data = try encoder.encode(foundText)
+                        try fileHandle.seekToEnd()
+                        try fileHandle.write(contentsOf: data)
+                        print("Text written successfully!")
+                    }
+                    try fileHandle.close()
+                    
+                } catch {
+                    print("Error writing to file: \(error)")
+                }
+
+
+                
+            }
+        }
+        
+        func sendToServer(){
+            let url = URL(string: "http://192.168.1.230:3000/")!
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                guard let httpResponse = response as? HTTPURLResponse,
+                    (200...299).contains(httpResponse.statusCode) else {
+                    print(response)
+                    return
+                }
+                if let mimeType = httpResponse.mimeType, mimeType == "text/html",
+                    let data = data,
+                    let string = String(data: data, encoding: .utf8) {
+                    print(string)
+                }
+            }
+            task.resume()
         }
 
         
---
+++ NutritionCamera/FoundText.swift
@@ -0,0 +1,15 @@
+//
+//  FoundText.swift
+//  NutritionCamera
+//
+//  Created by Ryan Klein on 11/24/24.
+//
+
+import Foundation
+
+struct FoundText: Codable{
+    var text: String
+    var line: String
+    var lineTop: Float
+    var lineBottom: Float
+}
